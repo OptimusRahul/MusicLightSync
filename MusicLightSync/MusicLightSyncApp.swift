f5faa561e2ff441398a0ab110805c00d1b5bdc40
OptimusRahul <rockerahul97@gmail.com>
2025-04-06 22:54:34 +0530

    Initial Commit

---
+++ MusicLightSync/MusicLightSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicLightSyncApp.swift
+//  MusicLightSync
+//
+//  Created by Rahul Srivastava on 06/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicLightSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicLightSyncTests/MusicLightSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicLightSyncTests.swift
+//  MusicLightSyncTests
+//
+//  Created by Rahul Srivastava on 06/04/25.
+//
+
+import Testing
+@testable import MusicLightSync
+
+struct MusicLightSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
